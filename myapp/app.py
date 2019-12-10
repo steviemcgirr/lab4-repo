0460bc80c74fafcbe14b044e606776135e8c412f
Steven Mc Girr <stevenmcgirr@Stevens-MacBook-Pro.local>
2019-12-10 16:32:44 +0000

    changes

--- myapp/app.py
+++ myapp/app.py
@@ -1,10 +1,10 @@
 from flask import Flask,  render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template('index.html', methods=['GET', 'POST'])
-@app.route("/index.html")
+    return render_template('index.html')
+@app.route("/index.html", methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 @app.route("/contact-me.html")
