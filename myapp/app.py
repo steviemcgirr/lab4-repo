7cea879939ea521c5b958d30e61644200a600993
Steven Mc Girr <stevenmcgirr@Stevens-MacBook-Pro.local>
2019-12-10 13:15:20 +0000

    file changes

--- myapp/app.py
+++ myapp/app.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template('index.html')
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+@app.route("/contact-me.html")
+def contact():
+    return render_template('contact-me.html')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
